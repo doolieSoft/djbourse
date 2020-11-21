29392b68f283a195c1d7e013ba6c351da9d11d0d
Stefano <s.crapanzano@gmail.com>
2020-11-21 14:25:41 +0100

    remove tracking of update_prices.bat

--- update_prices.bat
+++
@@ -1,6 +0,0 @@
-
-echo off
-call C:\Users\c158492\ProjetPerso\djbourse\venv\Scripts\activate.bat
-
-call python C:\Users\c158492\ProjetPerso\djbourse\update_prices.py --api_key "xxx" --folder_data C:\Users\c158492\ProjetPerso\djbourse\data
-call C:\Users\c158492\ProjetPerso\djbourse\venv\Scripts\deactivate.bat
