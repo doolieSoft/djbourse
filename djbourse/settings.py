32b109120b556827dfaa991c26e4b595cbb0748a
Stefano Crapanzano <s.crapanzano@gmail.com>
2021-01-14 17:28:59 +0100

    sorting adapted for float

--- djbourse/settings.py
+++ djbourse/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'nbvi$270j^y@!=rf$4pid$w*)__x(@w9%s@tej#+z+%6p_@9m3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "192.168.0.3", "localhost"]
 
 # Application definition
 
