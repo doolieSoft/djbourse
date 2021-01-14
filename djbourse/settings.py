0996d07672e1090b5daa33a4af3595ce502d210d
Stefano Crapanzano <s.crapanzano@gmail.com>
2021-01-14 14:02:38 +0100

    added datatables from bootstrap for the transactions list

--- djbourse/settings.py
+++ djbourse/settings.py
@@ -105,7 +105,7 @@ TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
-USE_L10N = True
+USE_L10N = False
 
 USE_TZ = True
 
@@ -113,3 +113,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+DATE_FORMAT = 'Y/m/d'
